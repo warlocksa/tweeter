8b532a1c10fdd8478f09ac2780869e9078ac2f69
warlocksa <warlock_sa@hotmail.com>
2022-03-28 17:53:53 -0600

    jquery works and count the length of elemnts typing

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+// jQuery()
+// jQuery === $
+
+$(document).ready(function () {
+  // --- our code goes here ---
+  // make sure jquary is work.
+  console.log('jQuary is works');
+
+  // count the length of elements typing
+  $('#tweet-text').on('input', function (elements) { 
+    const input = elements.target.value.length
+    $(".counter").text(140 - input)
+  //if the input more than 140, the count change to red 
+    if (140 - input < 0) {
+      $(".counter").addClass("red")
+    }
+    else {
+      $(".counter").removeClass("red")
+    }
+  });
+});
+
